5afdf86f2f5e398632d145fae6ccff0b9a861282
Isaac Randel <isaacr@nocturne.net.nz>
2017-05-23 20:18:27 +1200

    basic project setup

---
+++ rapbot.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from rapbot import hello
+
+if __name__ == '__main__':
+    hello()
---
+++ rapbot/__init__.py
@@ -0,0 +1,14 @@
+
+# start
+## 1) start twitter listening interface
+## 2) when new tweet, process()
+## 3) process_tweet()
+## 4) update database with tweet
+## 5) convert tweet to something rapable ?????
+## 6) rap it (play)
+
+def main():
+
+
+def hello():
+    print('hello')
