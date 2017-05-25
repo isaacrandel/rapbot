b3e875a68fc9ce99bde865daa3f49836ab27a976
Isaac Randel <isaacr@nocturne.net.nz>
2017-05-25 20:10:24 +1200

    Add todo

--- rapbot/twitterapi.py
+++ rapbot/twitterapi.py
@@ -6,11 +6,11 @@ import twitter
 
 config = configparser.ConfigParser()
 ROOT_DIR = os.path.dirname(sys.modules['__main__'].__file__)
-print(ROOT_DIR)
 
 config.read(os.path.join(ROOT_DIR, 'rapbot.conf'))
 
-
+# TODO: replace with:
+# http://docs.tweepy.org/en/v3.4.0/streaming_how_to.html
 api = twitter.Api(
 	consumer_key=config['twitter']['consumer_key'],
 	consumer_secret=config['twitter']['consumer_secret'],
