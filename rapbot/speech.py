08d6a17820f4d756d4ef45d623a57364a7cf9311
Bayard Randel <kit@nocturne.net.nz>
2017-06-25 16:05:50 +1200

    Switch from pyttsx to espeak. (#2)

--- rapbot/speech.py
+++ rapbot/speech.py
@@ -1,21 +1,19 @@
 import random
 
-import pyttsx
+import espeak
 
 INTROS = ['yo yo yo', 'wassup homie its rapbot', 'its ya boi rapbot']
 OUTROS = ['Peace out homies', 'mic drop', 'laters boi']
+
 
 def build_rap(tweet):
     intro = random.choice(INTROS)
     outro = random.choice(OUTROS)
     return intro + tweet + outro
 
+
 def rap(tweet):
-    engine = pyttsx.init()
-    rate = engine.getProperty('rate')
-    engine.setProperty('rate', rate - 40)
-    
     rap = build_rap(tweet)
-    engine.say(rap)
-    engine.runAndWait()
-    
+
+    es = espeak.ESpeak(pitch=1)
+    es.say(rap)
