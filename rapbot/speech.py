a7bc107c6446214c5e0563c677f247b2625739ea
Isaac Randel <isaacr@nocturne.net.nz>
2017-06-25 17:53:54 +1200

    Cleanup imports

--- rapbot/speech.py
+++ rapbot/speech.py
@@ -6,17 +6,19 @@ import espeak
 from pydub import AudioSegment
 from pydub.playback import play
 
+
 INTROS = ['yo yo yo', 'whats up homie its rapbot',
           'its ya boi rapbot', 'should out to my homies from balmakewen intermediate',
           'Droppin this fire mix for room 15']
 OUTROS = ['Peace out homies', 'mic drop', 'laters boi', 'word to your motha']
 
+
 def get_loop():
-    from os import listdir
-    from os.path import isfile, join
     loop_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'loops')
-    loops = [os.path.join(loop_path, f) for f in listdir(loop_path) if isfile(join(loop_path, f))]
+    loops = [os.path.join(loop_path, f) for f in os.listdir(loop_path) if os.path.isfile(
+        os.path.join(loop_path, f))]
     return random.choice(loops)
+
 
 def build_rap(tweet):
     intro = random.choice(INTROS)
